f9e6b32be449275f346fa05a04e17b744a146abe
superminsu <msjang6870@gmail.com>
2023-12-06 17:58:22 +0900

    first commit

---
+++ dockerTest/src/main/java/com/docker/demo/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.docker.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/test")
+public class TestController {
+
+	@GetMapping("docker")
+	public String testDocker() {
+		return "test for docker start";
+	}
+}
